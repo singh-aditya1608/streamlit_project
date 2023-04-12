594b72eb5a1dcfd44e188db106c9bc19b07f84a9
Aditya Prakash Singh <aditya@Adityas-MacBook-Pro.local>
2023-04-12 21:20:49 +0530

    changes

--- app.py
+++ app.py
@@ -87,7 +87,7 @@ with st.container():
 #--------- Use local CSS------
 def local_css(file_name):
     with open(file_name) as f:
-        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=False)
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 local_css("style/style.css")
 
